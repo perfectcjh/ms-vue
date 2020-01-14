36fa6de1f77dbacb1548de7b2ba0cc11592b15de
perfectcjh <perfectcjh@foxmail.com>
2020-01-14 09:35:15 +0800

    update router

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -54,7 +54,7 @@
         :loading="loading"
         type="primary"
         style="width:100%; margin-bottom:30px;"
-        @click.native.prevent="handleLogin"
+        @click.stop="handleLogin"
       >
         {{ $t('login.btn') }}
       </el-button>
@@ -101,17 +101,6 @@ export default class extends Vue {
   private redirect?: string
   private otherQuery: Dictionary<string> = {}
 
-  @Watch('$route', { immediate: true })
-  private onRouteChange(route: Route) {
-    // TODO: remove the "as Dictionary<string>" hack after v4 release for vue-router
-    // See https://github.com/vuejs/vue-router/pull/2050 for details
-    const query = route.query as Dictionary<string>
-    if (query) {
-      this.redirect = query.redirect
-      this.otherQuery = this.getOtherQuery(query)
-    }
-  }
-
   mounted() {
     if (this.loginForm.username === '') {
       (this.$refs.username as Input).focus()
@@ -132,30 +121,19 @@ export default class extends Vue {
   }
 
   private handleLogin() {
-    (this.$refs.loginForm as ElForm).validate(async(valid: boolean) => {
+    (this.$refs.loginForm as ElForm).validate((valid: boolean) => {
       if (valid) {
         this.loading = true
-        await UserModule.Login(this.loginForm)
+        // await UserModule.Login(this.loginForm)
         this.$router.push({
           path: this.redirect || '/',
           query: this.otherQuery
         })
-        setTimeout(() => {
-          this.loading = false
-        }, 0.5 * 1000)
+        this.loading = false
       } else {
         return false
       }
     })
-  }
-
-  private getOtherQuery(query: Dictionary<string>) {
-    return Object.keys(query).reduce((acc, cur) => {
-      if (cur !== 'redirect') {
-        acc[cur] = query[cur]
-      }
-      return acc
-    }, {} as Dictionary<string>)
   }
 }
 </script>
